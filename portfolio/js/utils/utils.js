df5cfb4d760476cc0cad7452a329706bc775d625
uchang7194 <suyoung7194@gmail.com>
2017-11-11 17:31:18 +0900

    [Portfolio site] wave효과 만들기

---
+++ portfolio/js/utils/utils.js
@@ -0,0 +1,74 @@
+(function(global){
+  'use strict';
+
+  if( !!global.Utils ) {
+    throw 'Already utils object is exist';
+  }
+
+  global.Utils = function() {
+
+    /**
+     * @method typeValidation
+     * @description 데이터의 타입을 판별하는 함수
+     * @param target type all
+     * @param type string
+     */
+    // validation
+    var typeValidation = function(target, type) {
+      type = type.toLowerCase();
+      return (typeof target === type)
+    }
+    /**
+     * @method isElement
+     * @description element 요소인지 판별하는 함수
+     * @param el element
+     */
+    var isElement = function(el) {
+      
+      if( typeValidation(el, 'object') ) {
+        throw el + 'is not Object';
+      }
+      return (el.nodeType === 1)
+    }
+
+    /**
+     * @method extendsOption
+     * @description 기존 options에 새로운 option의 값을 합치는 함수.
+     * @param old_options 기존 options
+     * @param new_options 새로운 options
+     */
+    var extendsOption = function(old_options, new_options) {
+
+      var prop = null;
+
+      for( prop in new_options ) {
+
+        if( old_options.hasOwnProperty(prop) ) {
+          console.log('prop: ', prop);
+          console.log('prop: ', new_options[prop]);
+          old_options[prop] = new_options[prop];
+        }
+      }
+    }
+    /**
+     * @method getComputedStyle
+     * @description element의 스타일 객체를 반환하는 함수.
+     * @param element element 객체
+     */ 
+    var getComputedStyle = function(element) {
+
+      return global.getComputedStyle(element);
+    }
+
+    return {
+      // VALIDATION
+      typeValidation: typeValidation,
+      isElement: isElement,
+      // EXTENDS
+      extendsOption: extendsOption,
+      // GET
+      getComputedStyle: getComputedStyle
+    }
+  }();
+
+}(window));
---
+++ portfolio/js/waves.js
@@ -0,0 +1,184 @@
+(function(global, U){
+  'use strict';
+
+  if( !!window.Wave ) {
+    throw 'Already Wave object is exist'
+  }
+
+  global.Wave = function() {
+
+    var target_obj = null,
+        wave_box = null,
+        waves = [];
+
+    var options = {
+      wave_box_color: '#feb47b',
+      wave_speed: 3000,
+      wave_pulse: 1500,
+      wave_colors: [
+        '#dce35b',
+        '#45b649',
+      ]
+    }
+
+    var resizeTimeout = null;
+
+    return function(target, new_options){
+      
+      /**
+       * @method init
+       * @description Wave 생성자를 초기화시키는 함수.
+       */ 
+      function init() {
+        if( U.typeValidation(target, 'string') ) {
+          target_obj = document.querySelector(target);     
+        } 
+
+        if( U.typeValidation(target, 'object') ) {
+          target_obj = target;
+        }
+
+        if( U.typeValidation(new_options, 'undefined') ) {
+          options = options || new_options;
+        } else {
+          U.extendsOption(options, new_options);
+        }
+
+        createWaveElement();
+        setWaveStyle();
+        bind();
+      }
+      /**
+       * @method bind
+       * @description 이벤트 바인딩을 모아놓는 함수.
+       */
+      function bind() {
+        // 1. window resize(wave)
+
+        global.addEventListener('resize', resizeFn);
+      }
+      /**
+       * @method resizeFn
+       * @methodOf bind
+       * @description window resize 이벤트시 작동하는 로직의 모음.
+       */ 
+
+      function resizeFn() {
+        global.clearTimeout(resizeTimeout);
+        resizeTimeout = global.setTimeout(function() {
+          setWaveSize();
+        }, 200)
+      }
+
+      /**
+       * @method createWaveElement
+       * @methodOf init
+       * @description wave 엘리먼트를 생성하는 함수.
+       */
+      function createWaveElement() {
+        /*
+          target 
+            - div(.wave-box)
+              - div(.wave one)
+              - div(.wave two)
+              - div(.wave three)
+        */
+
+        var fragment = document.createDocumentFragment(),
+            _wave_box = document.createElement('div');
+
+        var wave_colors = options.wave_colors;
+        _wave_box.setAttribute('class', 'wave-box');
+
+
+        for(var i = 0, len = wave_colors.length; i < len; i++) {
+          var _wave = document.createElement('div');
+
+          _wave.setAttribute('class', 'wave ' + i);
+          
+          _wave_box.appendChild(_wave);
+          waves.push(_wave);
+          
+        }
+        
+        wave_box = _wave_box;
+        fragment.appendChild(_wave_box);
+        target_obj.appendChild(fragment);
+        
+      }
+      
+      /**
+       * @function setWaveStyle
+       * @description wave style을 정의하는 함수.
+       */ 
+      function setWaveStyle() {
+        // 1. setWaveColor
+        setWaveColorAndSpeed();
+        // 2. wave size
+        setWaveSize();
+      }
+
+      /**
+       * @function setWaveColorAndSpeed
+       * @methodOf setWaveStyle
+       * @description wave color와 speed를 정의하는 함수.
+       */
+      function setWaveColorAndSpeed() {
+        // wave-box, waves style value(speed, color)
+        var wave_speed = options.wave_speed,
+            wave_pulse = options.wave_pulse;
+
+        var wave_box_color = options.wave_box_color,
+            wave_colors = options.wave_colors,
+            new_wave_box_style = {
+              background: wave_box_color 
+            };
+
+        // wave-box style extends new style
+        U.extendsOption(wave_box.style, new_wave_box_style);
+
+        for(var i = 0, len = waves.length; i < len; i++) {
+          var wave = waves[i];
+          var new_wave_style = {
+            background: wave_colors[i],
+            animationDuration: wave_speed + 'ms'
+          }
+          // console.log(new_wave_style);
+          wave_speed += wave_pulse;
+          U.extendsOption(wave.style, new_wave_style);
+        }
+      }
+      /**
+       * @method setWaveSize
+       * @methodOf setWaveStyle
+       * @description wave 엘리먼트의 사이즈를 정의하는 함수.
+       */
+      function setWaveSize() {
+
+        var wave_box_style = U.getComputedStyle(wave_box);
+
+        var wave_box_width = parseInt(wave_box_style.width, 10),
+            wave_box_height = parseInt(wave_box_style.height, 10),
+            wave_box_size = 0;
+
+        if( wave_box_width >= wave_box_height ) {
+          wave_box_size = wave_box_width * 1.2;
+        } else {
+          wave_box_size = wave_box_height * 1.2;
+        }
+        
+        for(var i = 0, len = waves.length; i < len; i++) {
+          var _wave = waves[i];
+          var new_wave_style = {
+            width: wave_box_size + 'px',
+            height: wave_box_size + 'px',
+            marginLeft: -(wave_box_size / 2)  + 'px'
+          }
+          U.extendsOption(_wave.style, new_wave_style);
+        }
+      }
+      init();
+    };
+  }();
+  
+}(window, window.Utils));
